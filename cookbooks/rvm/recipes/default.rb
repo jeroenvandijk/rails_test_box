4454a8e8e4804a938947b814a6c78a8db031aca0
Jeroen van Dijk <jeroen@jeevidee.nl>
2010-05-10 22:10:20 +0200

    Working around RVM 'feature' that's not helping us

--- cookbooks/rvm/recipes/default.rb
+++ cookbooks/rvm/recipes/default.rb
@@ -5,12 +5,15 @@ rvm = "#{rvm_home}/bin/rvm"
 bash "install rvm" do
   user "vagrant"
   # Using the example command from http://rvm.beginrescueend.com
+  # FIXME RVM does not work for us after the commit below. Find a better solution. 
+  # Commit can be found here http://github.com/wayneeseguin/rvm/commit/852aa1e117034bebcdc0b4e930b4e29165bc0473
   code <<-EOH
     mkdir -p ~/.rvm/src/ 
     cd ~/.rvm/src
     rm -rf ./rvm/
-    git clone --depth 1 git://github.com/wayneeseguin/rvm.git
-    cd rvm 
+    git clone git://github.com/wayneeseguin/rvm.git
+    cd rvm
+    git checkout 852aa1e117034bebcdc0b4e930b4e29165bc0473
     ./install
   EOH
   not_if { File.exists?(rvm_home) }
